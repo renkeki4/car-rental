ccd986c6d8dc429c3c00b0cad8342abf9b644630
renkeki4 <lastep1001@mail.ru>
2026-07-05 23:27:31 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,7 +1,6 @@
 from app import app
 from flask import render_template
 
-# Мы перенесём маршруты сюда позже, а пока оставим заглушку
 @app.route('/')
 def index():
     return 'Приложение работает! (routes)'
