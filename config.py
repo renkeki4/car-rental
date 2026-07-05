9e359ed49654646161b5b64a384af2e4626fd423
renkeki4 <lastep1001@mail.ru>
2026-07-05 23:28:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     
-    # Путь к базе данных (создаём папку instance)
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     INSTANCE_DIR = os.path.join(BASE_DIR, 'instance')
     if not os.path.exists(INSTANCE_DIR):
@@ -14,9 +13,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(INSTANCE_DIR, "rental.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    # Настройки для загрузки фото
     UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'uploads')
-    # Создаём папку, если её нет
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB
